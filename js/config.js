850ba9683a7f468a23b930b4030c956316ae3de3
Allison Higginbotham <higgintop@gmail.com>
2015-07-14 09:07:25 -0400

    Added other routes in the config

--- js/config.js
+++ js/config.js
@@ -11,6 +11,21 @@ app.config(['$routeProvider',
         controller: 'LandingPageController',
         css: '../css/partials/archive.css'
       }).
+      when('/about-artist', {
+        templateUrl: '../views/partials/about-artist.html',
+        controller: 'LandingPageController',
+        css: '../css/partials/about-artist.css'
+      }).
+      when('/todays-comic', {
+        templateUrl: '../views/partials/todays-comic.html',
+        controller: 'LandingPageController',
+        css: '../css/partials/todays-comic.css'
+      }).
+      when('/art-for-sale', {
+        templateUrl: '../views/partials/art-for-sale.html',
+        controller: 'LandingPageController',
+        css: '../css/partials/art-for-sale.css'
+      }).
       otherwise({
         redirectTo: '/'
       });
